9bf1b4f6596c3a94051936b98a6b1445257aa342
Robbie Booth <robbie.booth.2021@uni.strath.ac.uk>
2024-08-06 15:15:13 +0100

    added instructions to home

--- server.py
+++ server.py
@@ -409,7 +409,7 @@ def is_empty(value: str | None) -> bool:
 
 @app.route("/")
 def home():
-    return render_template("home.html", session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
+    return render_template("home.html")
 
 
 @app.route('/saveanswers/<int:id>', methods=['POST'])
